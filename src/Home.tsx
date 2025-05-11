c79f764a188f9c22ca70d159df610ca087c30e23
kota kawakami <kota160815@gmail.com>
2025-05-11 17:35:26 +0900

    modified

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,4 @@
-import "./UploadFile.css"
+import "./Home.css"
 import { Header } from "./components/Header";
 import { Link } from "react-router-dom";
 import ExcelIcon from "./assets/kkrn_icon_excel_5.svg";
@@ -10,33 +10,38 @@ export const Home = () =>{
     return(
         <>
             <Header/>
-            <div className="home-message">
-                ドキュメントを運ぶ、小さな羽ばたき。<br />
-                ToriDocsでやさしくつながる。
+            <div className="container">
+                <div className="home-layout">
+                    <div className="home-message">
+                        ドキュメントを運ぶ、小さな羽ばたき。<br />
+                        ToriDocsでやさしくつながる。
+                    </div>
+
+                    <div className="upload-buttons">
+                        <Link to="/upload_file/word" state={{ folder: "word" }}>
+                        <button className="icon-button">
+                            <img src={WordIcon} alt="画像アイコン" width={200} height={200} />
+                            <span>Wordファイル</span>
+                        </button>
+                        </Link>
+
+                        <Link to="/upload_file/excel" state={{ folder: "excel" }}>
+                        <button className="icon-button">
+                            <img src={ExcelIcon} alt="画像アイコン" width={200} height={200} />
+                            <span>Excelファイル</span>
+                        </button>
+                        </Link>
+
+                        <Link to="/upload_file/pdf" state={{ folder: "pdf" }}>
+                        <button className="icon-button">
+                            <img src={PdfIcon} alt="画像アイコン" width={200} height={200} />
+                            <span>PDFファイル</span>
+                        </button>
+                        </Link>
+                    </div>
+                </div>
             </div>
-
-            <div className="upload-buttons">
-                <Link to="/upload_file/word" state={{ folder: "word" }}>
-                <button className="icon-button">
-                    <img src={WordIcon} alt="画像アイコン" width={40} height={40} />
-                    <span>Wordファイル</span>
-                </button>
-                </Link>
-
-                <Link to="/upload_file/excel" state={{ folder: "excel" }}>
-                <button className="icon-button">
-                    <img src={ExcelIcon} alt="画像アイコン" width={40} height={40} />
-                    <span>Excelファイル</span>
-                </button>
-                </Link>
-
-                <Link to="/upload_file/pdf" state={{ folder: "pdf" }}>
-                <button className="icon-button">
-                    <img src={PdfIcon} alt="画像アイコン" width={40} height={40} />
-                    <span>PDFファイル</span>
-                </button>
-                </Link>
-            </div>
+            
         </>
 
     );
